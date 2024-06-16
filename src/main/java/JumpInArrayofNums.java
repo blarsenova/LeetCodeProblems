88850be659cf3ac80e2951b42e835976b5c63db5
Bakulya <blarsenova@gmail.com>
2024-06-15 23:46:51 -0500

    Leetcode Proble to jump in array w for loop and condition

--- src/main/java/JumpInArrayofNums.java
+++ src/main/java/JumpInArrayofNums.java
@@ -1,7 +1,7 @@
 public class JumpInArrayofNums {
 
     public boolean canJump(int[] nums) {
-        int maxReach = 0; // Initialize the maximum reach to 0
+        int maxReach = 0;
         for (int i = 0; i < nums.length; i++) {
             if (i > maxReach) { // If the current index is greater than the maximum reachable index
                 return false;
@@ -10,8 +10,18 @@ public class JumpInArrayofNums {
                 maxReach = i + nums[i];
             }
         }
-        // Check if we can reach the last index
+
         return maxReach >= nums.length - 1;
     }
+    public static void main(String[] args) {
+        JumpInArrayofNums jump = new JumpInArrayofNums();
 
+        // Example 1
+        int[] nums1 = {2, 3, 1, 1, 4};
+        System.out.println(jump.canJump(nums1));
+
+        // Example 2
+        int[] nums2 = {3, 2, 1, 0, 4};
+        System.out.println(jump.canJump(nums2));
+    }
 }
