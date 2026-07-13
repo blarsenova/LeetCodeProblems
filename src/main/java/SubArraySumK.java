65ff569890861cdfbbf88273b369d37b5c2c8c17
Bakulya <blarsenova@gmail.com>
2026-07-12 20:12:08 -0500

    lc problem 560, week 16-4

---
+++ src/main/java/SubArraySumK.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubArraySumK {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // Initialize with sum 0 having one occurrence
+
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+    
+}
